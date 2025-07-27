189532964e02cdb3897161c6d5a2b6ef4da1830c
Bea M <64287361+beamariano@users.noreply.github.com>
2025-07-27 16:00:01 +0800

    feat: add static

---
+++ _next/static/chunks/45092124-1f07100d0880ee93.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[942],{8475:(t,a,e)=>{e.d(a,{nwe:()=>h});var c=e(3620);function h(t){return(0,c.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"},child:[]}]})(t)}}}]);
